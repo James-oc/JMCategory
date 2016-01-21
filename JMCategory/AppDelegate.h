860272a3d589ff8980eb39b6537e2dcf181f07b9
shengxiao <1007785739@qq.com>
2016-01-21 15:17:08 +0800

    Initial Commit

---
+++ JMCategory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JMCategory
+//
+//  Created by shengxiao on 16/1/21.
+//  Copyright © 2016年 shengxiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
